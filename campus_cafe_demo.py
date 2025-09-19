1c3c9d5177e3a69503d29146010f4911ae8c5f0e
rwiederblank <rwiederblank@student.touro.edu>
2025-09-19 11:42:01 -0400

    Add files via upload

---
+++ campus_cafe_demo.py
@@ -0,0 +1,16 @@
+Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 09:44:33) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\student\Desktop\Intro to programming\Module 3\campus_cafe.py
+How many coffees? 2
+How many muffins? 3
+Enter tip percent (e.g., 10 for 10%): 22
+---Receipt---
+2 x coffee @ $2.25 = $4.5
+3 x muffin @ $2.75 = $8.25
+Subtotal:  $12.75
+Tax(8.8751%): $1.13
+Tip(22%): $2.81
+TOTAL:  $12.75
+THANK YOU!
+>>> 
